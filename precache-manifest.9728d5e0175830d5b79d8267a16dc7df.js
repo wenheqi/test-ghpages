1027164aa9e1d20b73ecc866ef6f6a4cbf9bd074
wenheqi <wenhe.qi@outlook.com>
2020-05-12 21:03:25 -0700

    Updates

---
+++ precache-manifest.9728d5e0175830d5b79d8267a16dc7df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e862d36cbefcfc6db4b12b59affbd9b6",
+    "url": "/test-ghpages/index.html"
+  },
+  {
+    "revision": "e6727bbd66a44debc56d",
+    "url": "/test-ghpages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea4394acf0f6d1acf818",
+    "url": "/test-ghpages/static/js/2.40a1265f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-ghpages/static/js/2.40a1265f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6727bbd66a44debc56d",
+    "url": "/test-ghpages/static/js/main.d96ad3a3.chunk.js"
+  },
+  {
+    "revision": "68690c2d6d21e329a89f",
+    "url": "/test-ghpages/static/js/runtime-main.f78c1d28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-ghpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
